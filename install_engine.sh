6124f096facfaab1dc37022c0c073076e61fead3
alex-chepurnoy <59021023+alex-chepurnoy@users.noreply.github.com>
2025-01-03 15:38:14 -0500

    Update install_engine.sh
    Added more logic to JKS upload.
    3 functions manage the detection, upload of JKS file and configuration of SSL

--- install_engine.sh
+++ install_engine.sh
@@ -85,17 +85,134 @@ sudo docker cp temp_container:/usr/local/WowzaStreamingEngine/conf/Server.xml "$
 # Remove the temporary container
 sudo docker rm -f temp_container > /dev/null
 
-while true; do
-  read -p "Do you want to add your .jks file to $BASE_DIR? (y/n): " user_input
+# Function to scan for .jks file
+check_for_jks() {
+  echo "Files found in $BASE_DIR:"
+  ls -1 "$BASE_DIR"
 
-  case $user_input in
+  # Find the .jks file
+  jks_file=$(ls "$BASE_DIR"/*.jks 2>/dev/null | head -n 1)
+  if [ -z "$jks_file" ]; then
+    echo "No .jks file found."
+    upload_jks
+  else
+    jks_file=$(basename "$jks_file")
+    read -p "A .jks file ($jks_file) was detected. Do you want to use this file? (y/n): " use_detected_jks
+    case $use_detected_jks in
+      [Yy]* )
+        ssl_config
+        ;;
+      [Nn]* )
+        upload_jks
+        ;;
+      * )
+        echo "Please answer yes or no."
+        check_for_jks
+        ;;
+    esac
+  fi
+}
+
+# Function to configure SSL
+ssl_config() {
+  read -p "Provide the domain for .jks file (e.g., myWowzaDomain.com): " jks_domain
+  read -s -p "Please enter the .jks password (to establish https connection to Wowza Manager): " jks_password
+  echo
+
+  # Setup Engine to use SSL for streaming and Manager access #
+  # Create the tomcat.properties file
+  echo "   -----Creating tomcat.properties file-----"
+  cat <<EOL > "$BASE_DIR/tomcat.properties"
+httpsPort=8090
+httpsKeyStore=/usr/local/WowzaStreamingEngine/conf/${jks_file}
+httpsKeyStorePassword=${jks_password}
+#httpsKeyAlias=[key-alias]
+EOL
+
+  # Change the <Port> line to have only 1935,554 ports
+  sed -i 's|<Port>1935,80,443,554</Port>|<Port>1935,554</Port>|' "$BASE_DIR/VHost.xml"
+  
+  # Edit the VHost.xml file to include the new HostPort block with the JKS and password information
+  sed -i '/<\/HostPortList>/i \
+  <HostPort>\
+      <Name>Autoconfig SSL Streaming</Name>\
+      <Type>Streaming</Type>\
+      <ProcessorCount>\${com.wowza.wms.TuningAuto}</ProcessorCount>\
+      <IpAddress>*</IpAddress>\
+      <Port>443</Port>\
+      <HTTPIdent2Response></HTTPIdent2Response>\
+      <SSLConfig>\
+          <KeyStorePath>/usr/local/WowzaStreamingEngine/conf/'${jks_file}'</KeyStorePath>\
+          <KeyStorePassword>'${jks_password}'</KeyStorePassword>\
+          <KeyStoreType>JKS</KeyStoreType>\
+          <DomainToKeyStoreMapPath></DomainToKeyStoreMapPath>\
+          <SSLProtocol>TLS</SSLProtocol>\
+          <Algorithm>SunX509</Algorithm>\
+          <CipherSuites></CipherSuites>\
+          <Protocols></Protocols>\
+          <AllowHttp2>true</AllowHttp2>\
+      </SSLConfig>\
+      <SocketConfiguration>\
+          <ReuseAddress>true</ReuseAddress>\
+          <ReceiveBufferSize>65000</ReceiveBufferSize>\
+          <ReadBufferSize>65000</ReceiveBufferSize>\
+          <SendBufferSize>65000</SendBufferSize>\
+          <KeepAlive>true</KeepAlive>\
+          <AcceptorBackLog>100</AcceptorBackLog>\
+      </SocketConfiguration>\
+      <HTTPStreamerAdapterIDs>cupertinostreaming,smoothstreaming,sanjosestreaming,dvrchunkstreaming,mpegdashstreaming</HTTPStreamerAdapterIDs>\
+      <HTTPProviders>\
+          <HTTPProvider>\
+              <BaseClass>com.wowza.wms.http.HTTPCrossdomain</BaseClass>\
+              <RequestFilters>*crossdomain.xml</RequestFilters>\
+              <AuthenticationMethod>none</AuthenticationMethod>\
+          </HTTPProvider>\
+          <HTTPProvider>\
+              <BaseClass>com.wowza.wms.http.HTTPClientAccessPolicy</BaseClass>\
+              <RequestFilters>*clientaccesspolicy.xml</RequestFilters>\
+              <AuthenticationMethod>none</AuthenticationMethod>\
+          </HTTPProvider>\
+          <HTTPProvider>\
+              <BaseClass>com.wowza.wms.http.HTTPProviderMediaList</BaseClass>\
+              <RequestFilters>*jwplayer.rss|*jwplayer.smil|*medialist.smil|*manifest-rtmp.f4m</RequestFilters>\
+              <AuthenticationMethod>none</AuthenticationMethod>\
+          </HTTPProvider>\
+          <HTTPProvider>\
+              <BaseClass>com.wowza.wms.webrtc.http.HTTPWebRTCExchangeSessionInfo</BaseClass>\
+              <RequestFilters>*webrtc-session.json</RequestFilters>\
+              <AuthenticationMethod>none</AuthenticationMethod>\
+          </HTTPProvider>\
+          <HTTPProvider>\
+              <BaseClass>com.wowza.wms.http.HTTPServerVersion</BaseClass>\
+              <RequestFilters>*ServerVersion</RequestFilters>\
+              <AuthenticationMethod>none</AuthenticationMethod>\
+          </HTTPProvider>\
+      </HTTPProviders>\
+  </HostPort>' "$BASE_DIR/VHost.xml"
+
+  # Edit the VHost.xml file to include the new TestPlayer block with the jks_domain
+  sed -i '/<\/Manager>/i \
+  <TestPlayer>\
+      <IpAddress>'${jks_domain}'</IpAddress>\
+      <Port>443</Port>\
+      <SSLEnable>true</SSLEnable>\
+  </TestPlayer>' "$BASE_DIR/VHost.xml"
+  
+  # Edit the Server.xml file to include the JKS and password information
+  sed -i 's|<Enable>false</Enable>|<Enable>true</Enable>|' "$BASE_DIR/Server.xml"
+  sed -i 's|<KeyStorePath></KeyStorePath>|<KeyStorePath>/usr/local/WowzaStreamingEngine/conf/'${jks_file}'</KeyStorePath>|' "$BASE_DIR/Server.xml"
+  sed -i 's|<KeyStorePassword></KeyStorePassword>|<KeyStorePassword>'${jks_password}'</KeyStorePassword>|' "$BASE_DIR/Server.xml"
+  sed -i 's|<IPWhiteList>127.0.0.1</IPWhiteList>|<IPWhiteList>*</IPWhiteList>|' "$BASE_DIR/Server.xml"
+}
+
+# Function to upload .jks file
+upload_jks() {
+  read -p "Do you want to upload a .jks file? (y/n): " upload_jks
+  case $upload_jks in
     [Yy]* )
-      read -p "Press [Enter] to continue after uploading the files..." 
-      # List files found in the directory
-      echo "Files found in $BASE_DIR:"
-      ls -1 "$BASE_DIR"
-
       while true; do
+        read -p "Press [Enter] to continue after uploading the .jks file to $BASE_DIR..." 
+
         # Find the .jks file
         jks_file=$(ls "$BASE_DIR"/*.jks 2>/dev/null | head -n 1)
         if [ -z "$jks_file" ]; then
@@ -105,116 +222,32 @@ while true; do
               read -p "Press [Enter] to continue after uploading the files..."
               ;;
             [Nn]* )
-              echo "You chose not to add a .jks file."
-              break 2
+              echo "You chose not to add a .jks file. Moving on to tuning."
+              return 1
               ;;
             * )
               echo "Please answer yes or no."
               ;;
           esac
         else
-          jks_file=$(basename "$jks_file")
-          read -p "Provide the domain for .jks file (e.g., myWowzaDomain.com): " jks_domain
-          read -s -p "Please enter the .jks password (to establish https connection to Wowza Manager): " jks_password
-          echo
-
-          # Setup Engine to use SSL for streaming and Manager access #
-          # Create the tomcat.properties file
-          echo "   -----Creating tomcat.properties file-----"
-          cat <<EOL > "$BASE_DIR/tomcat.properties"
-httpsPort=8090
-httpsKeyStore=/usr/local/WowzaStreamingEngine/conf/${jks_file}
-httpsKeyStorePassword=${jks_password}
-#httpsKeyAlias=[key-alias]
-EOL
-
-          # Change the <Port> line to have only 1935,554 ports
-          sed -i 's|<Port>1935,80,443,554</Port>|<Port>1935,554</Port>|' "$BASE_DIR/VHost.xml"
-          
-          # Edit the VHost.xml file to include the new HostPort block with the JKS and password information
-          sed -i '/<\/HostPortList>/i \
-          <HostPort>\
-              <Name>Autoconfig SSL Streaming</Name>\
-              <Type>Streaming</Type>\
-              <ProcessorCount>\${com.wowza.wms.TuningAuto}</ProcessorCount>\
-              <IpAddress>*</IpAddress>\
-              <Port>443</Port>\
-              <HTTPIdent2Response></HTTPIdent2Response>\
-              <SSLConfig>\
-                  <KeyStorePath>/usr/local/WowzaStreamingEngine/conf/'${jks_file}'</KeyStorePath>\
-                  <KeyStorePassword>'${jks_password}'</KeyStorePassword>\
-                  <KeyStoreType>JKS</KeyStoreType>\
-                  <DomainToKeyStoreMapPath></DomainToKeyStoreMapPath>\
-                  <SSLProtocol>TLS</SSLProtocol>\
-                  <Algorithm>SunX509</Algorithm>\
-                  <CipherSuites></CipherSuites>\
-                  <Protocols></Protocols>\
-                  <AllowHttp2>true</AllowHttp2>\
-              </SSLConfig>\
-              <SocketConfiguration>\
-                  <ReuseAddress>true</ReuseAddress>\
-                  <ReceiveBufferSize>65000</ReceiveBufferSize>\
-                  <ReadBufferSize>65000</ReadBufferSize>\
-                  <SendBufferSize>65000</SendBufferSize>\
-                  <KeepAlive>true</KeepAlive>\
-                  <AcceptorBackLog>100</AcceptorBackLog>\
-              </SocketConfiguration>\
-              <HTTPStreamerAdapterIDs>cupertinostreaming,smoothstreaming,sanjosestreaming,dvrchunkstreaming,mpegdashstreaming</HTTPStreamerAdapterIDs>\
-              <HTTPProviders>\
-                  <HTTPProvider>\
-                      <BaseClass>com.wowza.wms.http.HTTPCrossdomain</BaseClass>\
-                      <RequestFilters>*crossdomain.xml</RequestFilters>\
-                      <AuthenticationMethod>none</AuthenticationMethod>\
-                  </HTTPProvider>\
-                  <HTTPProvider>\
-                      <BaseClass>com.wowza.wms.http.HTTPClientAccessPolicy</BaseClass>\
-                      <RequestFilters>*clientaccesspolicy.xml</RequestFilters>\
-                      <AuthenticationMethod>none</AuthenticationMethod>\
-                  </HTTPProvider>\
-                  <HTTPProvider>\
-                      <BaseClass>com.wowza.wms.http.HTTPProviderMediaList</BaseClass>\
-                      <RequestFilters>*jwplayer.rss|*jwplayer.smil|*medialist.smil|*manifest-rtmp.f4m</RequestFilters>\
-                      <AuthenticationMethod>none</AuthenticationMethod>\
-                  </HTTPProvider>\
-                  <HTTPProvider>\
-                      <BaseClass>com.wowza.wms.webrtc.http.HTTPWebRTCExchangeSessionInfo</BaseClass>\
-                      <RequestFilters>*webrtc-session.json</RequestFilters>\
-                      <AuthenticationMethod>none</AuthenticationMethod>\
-                  </HTTPProvider>\
-                  <HTTPProvider>\
-                      <BaseClass>com.wowza.wms.http.HTTPServerVersion</BaseClass>\
-                      <RequestFilters>*ServerVersion</RequestFilters>\
-                      <AuthenticationMethod>none</AuthenticationMethod>\
-                  </HTTPProvider>\
-              </HTTPProviders>\
-          </HostPort>' "$BASE_DIR/VHost.xml"
-
-          # Edit the VHost.xml file to include the new TestPlayer block with the jks_domain
-          sed -i '/<\/Manager>/i \
-          <TestPlayer>\
-              <IpAddress>'${jks_domain}'</IpAddress>\
-              <Port>443</Port>\
-              <SSLEnable>true</SSLEnable>\
-          </TestPlayer>' "$BASE_DIR/VHost.xml"
-          
-          # Edit the Server.xml file to include the JKS and password information
-          sed -i 's|<Enable>false</Enable>|<Enable>true</Enable>|' "$BASE_DIR/Server.xml"
-          sed -i 's|<KeyStorePath></KeyStorePath>|<KeyStorePath>/usr/local/WowzaStreamingEngine/conf/'${jks_file}'</KeyStorePath>|' "$BASE_DIR/Server.xml"
-          sed -i 's|<KeyStorePassword></KeyStorePassword>|<KeyStorePassword>'${jks_password}'</KeyStorePassword>|' "$BASE_DIR/Server.xml"
-          sed -i 's|<IPWhiteList>127.0.0.1</IPWhiteList>|<IPWhiteList>*</IPWhiteList>|' "$BASE_DIR/Server.xml"
-          break 2
+          check_for_jks
+          return 0
         fi
       done
       ;;
     [Nn]* )
-      echo "You chose not to add a .jks file."
-      break
+      echo "You chose not to add a .jks file. Moving on to tuning."
+      return 1
       ;;
     * )
       echo "Please answer yes or no."
+      upload_jks
       ;;
   esac
-done
+}
+
+# Handle JKS file detection and setup
+check_for_jks
 
 # Server Tuning #
 echo "   -----Tuning Network Sockets and Server Threads-----"
@@ -253,12 +286,12 @@ if [ "$demo_stream" = "y" ]; then
             </ServerListener>' "$BASE_DIR/Server.xml"
   
   # Find the line number of the closing </Properties> tag directly above the closing </Server> tag
-line_number=$(awk '/<\/Properties>/ {p=NR} /<\/Server>/ && p {print p; exit}' "$BASE_DIR/Server.xml")
+  line_number=$(awk '/<\/Properties>/ {p=NR} /<\/Server>/ && p {print p; exit}' "$BASE_DIR/Server.xml")
 
-# Insert the new property at the found line number
-if [ -n "$line_number" ]; then
-  sed -i "${line_number}i <Property>\n<Name>streamDemoPublisherConfig</Name>\n<Value>appName=live,srcStream=sample.mp4,dstStream=myStream,sendOnMetadata=true</Value>\n<Type>String</Type>\n</Property>" "$BASE_DIR/Server.xml"
-fi
+  # Insert the new property at the found line number
+  if [ -n "$line_number" ]; then
+    sed -i "${line_number}i <Property>\n<Name>streamDemoPublisherConfig</Name>\n<Value>appName=live,srcStream=sample.mp4,dstStream=myStream,sendOnMetadata=true</Value>\n<Type>String</Type>\n</Property>" "$BASE_DIR/Server.xml"
+  fi
 fi
 
 # Change directory to $BUILD_DIR/
